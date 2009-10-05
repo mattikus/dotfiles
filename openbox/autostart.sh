8bf5c9d75ab169b13f777a6f898e37e823b0d89b
Matt Kemp <matt@mattikus.com>
2009-10-05 12:33:22 -0500

    Updated openbox configs.

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -2,16 +2,18 @@
 if which dbus-launch >/dev/null && test -z "$DBUS_SESSION_BUS_ADDRESS"; then
     eval `dbus-launch --sh-syntax --exit-with-session`
 fi
-[[ -f $HOME/.fehbg ]] && eval $(cat $HOME/.fehbg)
-sleep 2 && pypanel &
-conky &
+[[ -f $HOME/.fehbg ]] && $(< $HOME/.fehbg)
 urxvtd -q -o -f
-gnome-screensaver
+orage &
+tint2 &
+#conky &
+#pytyle &
+#gnome-screensaver
 [[ -f $HOME/.laptopmode ]] && gpomme &
 [[ -f $HOME/.laptopmode ]] && syndaemon -d -i 0.45
 [[ -f $HOME/.laptopmode ]] || synergys
 [[ -f $HOME/.noxmodmap ]] || xmodmap $HOME/.xmodmaprc
 glipper-old &
-sonata --hidden --profile=1 &
-$HOME/.dropbox-dist/dropboxd &
+#sonata --hidden --profile=1 &
+#$HOME/.dropbox-dist/dropboxd &
 xcompmgr -c -r6 -Cf -l -9 -t -7 -D 2 &
